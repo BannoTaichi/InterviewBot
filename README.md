a70ffa2a960cd99d677b1f74d2eb48fe94e6e608
Banno Taichi <taichib@icloud.com>
2025-02-03 23:27:20 +0900

    Initialize Interview App

---
+++ README.md
@@ -0,0 +1,2 @@
+# 仮想環境
+- conda create -n interview python=3.10
---
+++ model/create_sentence.py
@@ -0,0 +1,29 @@
+import google.generativeai as genai
+
+# 取得したAPIキーを設定
+API_KEY = "AIzaSyCrV9QAgK9Y2d_qX7Po74PjPidTWd9eYCw"
+genai.configure(api_key=API_KEY)
+
+
+# Gemini プロンプト
+def generate_interview_questions(text, mode="自己PR"):
+    model = genai.GenerativeModel("gemini-pro")
+    prompt = f"""
+  以下の文章は、面接での{mode}です。この内容に基づいて、面接官が聞きそうな質問をセリフとして1つ出力してください。
+
+  【{mode}】
+  "{text}"
+
+  【想定質問】
+  """
+    response = model.generate_content(prompt)
+    return response.text
+
+
+if __name__ == "__main__":
+    # 自己PRを入力
+    user_input = "私は、大学の研究でガラスの上に銀薄膜の島状構造を作り、光増強性能を評価する研究を行っています。実験装置の改造を行い、実験条件を2倍に広げることに成功しました。"
+
+    # 質問生成
+    questions = generate_interview_questions(user_input)
+    print(questions)
